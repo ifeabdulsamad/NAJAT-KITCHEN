8f366c79bf54599964746b4f359640755ec14f16
LUIPHYX <akinpeluifeoluwaa@gmail.com>
2025-08-12 01:06:31 -0700

    Alhamdulillah

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>HELLO WORLD</h1>
+      <section>
+      <h3>Dersserts</h3>
+      <ul>
+        <li>
+          <img src="src/assets/IMAGES/image-baklava-desktop.jpg" alt="" />
+          <button>
+            Add to Cart
+          </button>
+          <h4>Indomie</h4>
+          <h4>$10</h4>
+        </li>
+        <li>
+          <img src="src/assets/IMAGES/image-brownie-desktop.jpg" alt="" />
+          <button>
+            Add to Cart
+          </button>
+          <h4>Indomie</h4>
+          <h4>$10</h4>
+        </li>
+        <li>
+          <img src="src/assets/IMAGES/image-cake-desktop.jpg" alt="" />
+          <button>
+            Add to Cart
+          </button>
+          <h4>Indomie</h4>
+          <h4>$10</h4>
+        </li>
+        <li>
+          <img src="src/assets/IMAGES/image-macaron-desktop.jpg" alt="" />
+          <button>
+            Add to Cart
+          </button>
+          <h4>Indomie</h4>
+          <h4>$10</h4>
+        </li>
+        <li>
+          <img src="src/assets/IMAGES/image-meringue-desktop.jpg" alt="" />
+          <button>
+            Add to Cart
+          </button>
+          <h4>Indomie</h4>
+          <h4>$10</h4>
+        </li>
+        <li>
+          <img src="src/assets/IMAGES/image-tiramisu-desktop.jpg" alt="" />
+          <button>
+            Add to Cart
+          </button>
+          <h4>Indomie</h4>
+          <h4>$10</h4>
+        </li>
+        <li>
+          <img src="src/assets/IMAGES/image-panna-cotta-desktop.jpg" alt="" />
+          <button>
+            Add to Cart
+          </button>
+          <h4>Indomie</h4>
+          <h4>$10</h4>
+        </li>
+        <li>
+          <img src="src/assets/IMAGES/image-waffle-desktop.jpg" alt="" />
+          <button>
+            Add to Cart
+          </button>
+          <h4>Indomie</h4>
+          <h4>$10</h4>
+        </li>
+      </ul>
+      </section>
+
+      <section>
+        <h5>Your Cart(7)</h5>
+        <img src="src/assets/IMAGES/illustration-empty-cart.svg" alt="" srcset="" />
+        <p>Your added items will apear here</p>
+      </section>
     </>
   )
 }
