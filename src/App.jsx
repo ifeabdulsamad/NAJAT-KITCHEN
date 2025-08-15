27f85f59a3692f1edac5e6835bc15e48b501e0bf
LUIPHYX <akinpeluifeoluwaa@gmail.com>
2025-08-15 07:48:15 +0100

    productjasonfile

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,24 @@
+import products from "./assets/prducts.json"
 function App() {
 
   return (
     <>
       <section>
       <h3>Dersserts</h3>
-      <ul>
-        <li>
-          <img src="src/assets/IMAGES/image-baklava-desktop.jpg" alt="" />
-          <button>
-            Add to Cart
-          </button>
-          <h4>Indomie</h4>
-          <h4>$10</h4>
-        </li>
-        <li>
-          <img src="src/assets/IMAGES/image-brownie-desktop.jpg" alt="" />
-          <button>
-            Add to Cart
-          </button>
-          <h4>Indomie</h4>
-          <h4>$10</h4>
-        </li>
-        <li>
-          <img src="src/assets/IMAGES/image-cake-desktop.jpg" alt="" />
-          <button>
-            Add to Cart
-          </button>
-          <h4>Indomie</h4>
-          <h4>$10</h4>
-        </li>
-        <li>
-          <img src="src/assets/IMAGES/image-macaron-desktop.jpg" alt="" />
-          <button>
-            Add to Cart
-          </button>
-          <h4>Indomie</h4>
-          <h4>$10</h4>
-        </li>
-        <li>
-          <img src="src/assets/IMAGES/image-meringue-desktop.jpg" alt="" />
-          <button>
-            Add to Cart
-          </button>
-          <h4>Indomie</h4>
-          <h4>$10</h4>
-        </li>
-        <li>
-          <img src="src/assets/IMAGES/image-tiramisu-desktop.jpg" alt="" />
-          <button>
-            Add to Cart
-          </button>
-          <h4>Indomie</h4>
-          <h4>$10</h4>
-        </li>
-        <li>
-          <img src="src/assets/IMAGES/image-panna-cotta-desktop.jpg" alt="" />
-          <button>
-            Add to Cart
-          </button>
-          <h4>Indomie</h4>
-          <h4>$10</h4>
-        </li>
-        <li>
-          <img src="src/assets/IMAGES/image-waffle-desktop.jpg" alt="" />
-          <button>
-            Add to Cart
-          </button>
-          <h4>Indomie</h4>
-          <h4>$10</h4>
-        </li>
-      </ul>
+          <div>
+      {products.map((product) => (
+        <div key={product.id}>
+          <img
+            src={product.img}
+          />
+        </div>
+      ))}
+    </div>
       </section>
 
       <section>
         <h5>Your Cart(7)</h5>
-        <img src="src/assets/IMAGES/illustration-empty-cart.svg" alt="" srcset="" />
+        <img src="src/assets/IMAGES/illustration-empty-cart.svg" alt="" />
         <p>Your added items will apear here</p>
       </section>
     </>
