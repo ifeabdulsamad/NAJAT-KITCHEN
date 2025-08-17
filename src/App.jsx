561608121ee9c6738e887ece944b7cceb1fb3175
LUIPHYX <akinpeluifeoluwaa@gmail.com>
2025-08-17 20:47:16 +0100

    ccc

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import products from "./prducts.json";
-import "./SStyles.css";
+import "./Styles.css";
 function App() {
   return (
     <main className="grid grid-cols-1 lg:grid-cols-12  gap-6 p-6 m-6 ">
